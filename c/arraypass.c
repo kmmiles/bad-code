97cbd668e2ec5816be14081e76470c1a35bbbc2a
kmmiles <kenneth.mmiles@gmail.com>
2021-09-01 02:05:42 -0600

    up

---
+++ c/arraypass.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void myfunc(int *bar, int len) {
+  for (int i = 0; i <= len; i++) {
+    printf("i: %d\n", i);
+  }
+}
+
+int main(void) {
+  int foo[] = {1, 2, 3, 4, 5, 6};
+  myfunc(foo, sizeof(foo) / sizeof(int));
+}
